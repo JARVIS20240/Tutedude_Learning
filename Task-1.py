3be906961f622e88c626abd739b9f569bbd23baf
Karan <157726166+JARVIS20240@users.noreply.github.com>
2025-04-10 23:41:50 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,15 @@
+# step-1 Takes two Numbers from user: 
+number_1=int(input("Enter a 1st Number: "))
+number_2=int(input("Enter a 2nd Number: "))
+
+# Some Mathematical Operation on Thoose two numbers:
+addition= number_1 + number_2
+subtraction= number_1 - number_2
+multiplication= number_1 * number_2
+division= number_1 / number_2
+
+#For a Output:
+print("Addition: ",addition)
+print("Substraction: ",subtraction)
+print("Multiplication: ",multiplication)
+print("Division: ",division)
---
+++ Task-2.py
@@ -0,0 +1,5 @@
+#Taking User First name and Last name as input:
+f_name=input("Enter your Firest name: ")
+l_name=input("Enter tour Last name:  ")
+
+print("Hello,",f_name + " " +l_name,"!"," Welcome to the Python program")
